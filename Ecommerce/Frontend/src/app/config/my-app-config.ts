ef3f75b27fda81b435f1e4e1eb6c680cf4cbb06f
Stefanescu Mircea <stefanescumircea1990@gmail.com>
2023-08-29 13:30:56 +0300

    front end

---
+++ Ecommerce/Frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa7rwnp7fkWQUZN75d7',
+        issuer: 'https://dev-60470360.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
---
+++ Ecommerce/Frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  luv2shopApiUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51MNIG8GR83GV0X5KBocameDitWSjWBTIGn22jN90FaynsXWdTsN6HWpJ9QJMg5ZgAn5NAEO82gdzPUtpZ9CPmy0Q00Nvv6raqP"
+};
